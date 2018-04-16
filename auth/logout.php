6ac550a15b808a5dd5901fb1331e192f63343a9c
Francisco Garcia <chisco420@gmail.com>
2018-04-16 17:05:48 -0600

    Initial commit

---
+++ auth/logout.php
@@ -0,0 +1,7 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Francisco
+ * Date: 4/16/2018
+ * Time: 3:41 PM
+ */
---
+++ auth/resource/Database.php
@@ -0,0 +1,10 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Francisco
+ * Date: 4/16/2018
+ * Time: 3:39 PM
+ */
+$db = new PDO('mysql:host=localhost; dbname=register', 'yoga', '1234');
+
+echo "Connected to the REGISTER database";
