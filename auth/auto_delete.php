dd39ea3af6abba3dfd299fbaf28b7692530d410a
Francisco Garcia <chisco420@gmail.com>
2018-05-01 18:44:14 -0600

    AutoDelete features

--- auth/auto_delete.php
+++ auth/auto_delete.php
@@ -27,15 +27,46 @@ try
                 unlink($user_pic);
             }
 
-            $db ->exec("DELETE FROM deactivatedusers WHERE user_id = $id LIMIT 1");
+            $result = $db ->exec("DELETE FROM deactivatedusers WHERE user_id = $id LIMIT 1");
             $db ->exec("DELETE FROM users WHERE id = $id AND activated = '0' LIMIT 1");
 
             //EMAIL OR LOG
-            //echo "Account deleted";
+            echo "$result Account deleted";
         }
     }
 }
 catch (PDOException $ex)
+{
+    //email yourself or log it
+    //$ex->getMessage();
+}
+
+try
+{
+    debug_to_console("DELETE Non activades users");
+    //Non activated users
+    $sqlQueryNA = $db->query("SELECT id, username FROM users WHERE join_date <= CURRENT_DATE - INTERVAL 3 DAY AND activated = '0'");
+
+    while($rs = $sqlQueryNA->fetch())
+    {
+        //get records
+        $user_id = $rs['id'];
+        $user_name = $rs['username'];
+
+        $userRecord= $db->prepare("SELECT * from users WHERE id = :id");
+        $userRecord -> execute(array(':id'=> $user_id));
+
+        if (!checkDuplicateEntries('deactivatedusers', 'user_id', $user_id, $db))
+        {
+
+            $result = $db ->exec("DELETE FROM users WHERE id = $user_id AND activated = '0' LIMIT 1");
+
+            //EMAIL OR LOG
+            echo "$result Account deleted";
+        }
+    }
+}
+catch (PDOException $e)
 {
     //email yourself or log it
     //$ex->getMessage();
